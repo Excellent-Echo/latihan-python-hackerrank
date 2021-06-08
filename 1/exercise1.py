c6fc8bf5d0b8854053b8883bdc449e5151403cc6
efzedel16 <efzedel16@outlook.com>
2021-06-08 15:06:07 +0700

    Exercise 1 : Sock Merchant has been complete

---
+++ 1/exercise1.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+def sock_merchant(n, ar):
+    unique_color = set(ar)
+    amount_color = {color: 0 for color in unique_color}
+    for color in ar:
+        amount_color[color] += 1
+    amount_pairs = 0
+    for color in unique_color:
+        amount_pairs += amount_color[color]//2
+    return amount_pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = sock_merchant(n, ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
