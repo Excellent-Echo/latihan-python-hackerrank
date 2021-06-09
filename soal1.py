c9ff9aa8d47cb75d3dab122be875374642a5991d
Angga Prasetya W <agpw.id@gmail.com>
2021-06-09 10:41:34 +0800

    soal 1,2,4

---
+++ soal1.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    socks={}
+    count=0
+    
+    for i in ar:
+        if i not in socks:
+            socks[i]=1
+            continue
+        socks[i]+=1
+    
+    for i in socks.values():
+        count+=i//2
+    return count    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
